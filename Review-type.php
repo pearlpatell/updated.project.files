0f9c44d27c5cc49a86be1a5a53e1114dbc702535
parkerzaharis <125917982+parkerzaharis@users.noreply.github.com>
2023-03-08 16:39:46 +0100

    Add files via upload

---
+++ Review-type.php
@@ -0,0 +1,21 @@
+</html>
+
+<html>
+
+<head>
+	<title> Find Good Food </title>
+</head>
+<body>
+<h1>
+Thank you for leaving a review!
+</h1>
+
+<h2>
+Which kind of review would you like to leave?
+</h2>
+<a href="Add-food-review.php">Food Review</a><br>
+<a href="Add-restaurant-review.php">Restaurant Review</a>
+</body>
+
+
+</html>
